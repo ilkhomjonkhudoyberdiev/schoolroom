cf6a9da9428570c133ac4877ecb61ed13a4cba06
Ilkhomjon Khudoyberdiev <ilhom_Xudoyberdiyev@mail.ru>
2024-03-31 22:16:10 +0100

    Commit initial files

---
+++ src/test/java/com/schoolroom/SchoolroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
